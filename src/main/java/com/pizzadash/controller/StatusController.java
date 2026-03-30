1379f449b959246209cacb17a1876bfb86cb1607
Lucasgastaop <lucagastaopinheiro@gmail.com>
2026-03-30 19:32:16 -0300

    Sjuste na api de HealthCheck

--- src/main/java/com/pizzadash/controller/StatusController.java
+++ src/main/java/com/pizzadash/controller/StatusController.java
@@ -3,10 +3,12 @@ package com.pizzadash.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/health")
+@RestController
+@RequestMapping("/health")
 public class StatusController {
 	
-    @GetMapping public String check() {
+    @GetMapping 
+    public String check() {
         return "PizzaDash API Online";
     }
     
